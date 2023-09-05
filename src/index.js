edfcc291fcba8cfc98a4e517ef28135f9fee22c5
Soniq7 <ssoniq7@gmail.com>
2023-09-05 22:47:16 +0200

    Globalne style dodane do body

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from "./core/App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../common/theme";
+import { GlobalStyle } from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
