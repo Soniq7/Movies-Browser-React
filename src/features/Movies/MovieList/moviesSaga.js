259e3660d235522d74c8dbacc4adeff4f67eac1c
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-06 16:48:37 +0200

    usunięto console logi i obsłużono możliwy error

--- src/features/Movies/MovieList/moviesSaga.js
+++ src/features/Movies/MovieList/moviesSaga.js
@@ -1,15 +1,17 @@
 import { getPopularMovies } from "./getPopularMovies";
-import { fetchMovies, fetchMoviesSuccess } from "./moviesSlice";
+import {
+  fetchMovies,
+  fetchMoviesError,
+  fetchMoviesSuccess,
+} from "./moviesSlice";
 import { call, takeLatest, put } from "redux-saga/effects";
 
 function* fetchMoviesHandler() {
   try {
     const movies = yield call(getPopularMovies);
-    console.log(movies);
     yield put(fetchMoviesSuccess(movies));
-    console.log("1");
   } catch {
-    console.log("error");
+    yield put(fetchMoviesError());
   }
 }
 
