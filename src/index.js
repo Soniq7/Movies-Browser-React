0f8018676b26c6a55db5c081f2eb026492ddbcf9
Soniq7 <ssoniq7@gmail.com>
2023-09-07 08:31:41 +0200

    ścieżka theme poprawiona

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./core/App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
-import { theme } from "../common/theme";
+import { theme } from "./common/theme";
 import { GlobalStyle } from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
