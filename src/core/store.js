67382925495e03e6bbb9fa65664809e730fa638c
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-07 21:01:54 +0200

    poprawiono literówki

--- src/core/store.js
+++ src/core/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import moviesReducer from "../features/Movies/MovieList/moviesSlice";
-import peoleReducer from "../features/People/PeopleList/peopleSlice";
+import peopleReducer from "../features/People/PeopleList/peopleSlice";
 import { rootSaga } from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -9,7 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     movies: moviesReducer,
-    people: peoleReducer,
+    people: peopleReducer,
   },
   middleware: [sagaMiddleware],
 });
