9587ff01e46ad97a5f1cff910236b4f51a82b81b
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-07 20:58:45 +0200

    dodano peopleSaga do głównej sagi

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,6 +1,7 @@
 import { moviesSaga } from "../features/Movies/MovieList/moviesSaga";
+import { peopleSaga } from "../features/People/PeopleList/peopleSaga";
 import { all } from "redux-saga/effects";
 
 export function* rootSaga() {
-  yield all([moviesSaga()]);
+  yield all([moviesSaga(), peopleSaga()]);
 }
