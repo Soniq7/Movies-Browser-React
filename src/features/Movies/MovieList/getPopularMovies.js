cc859bb60842f6aa7cbe4eaa83191137f61d058c
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-10 10:06:18 +0200

    usunięto filmy dla dorosłych

--- src/features/Movies/MovieList/getPopularMovies.js
+++ src/features/Movies/MovieList/getPopularMovies.js
@@ -11,7 +11,7 @@ const options = {
 
 export const getPopularMovies = async () => {
   const response = await axios.get(
-    "https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_cast=false&with_companies=false&with_crew=false",
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_cast=false&with_companies=false&with_crew=false",
     options
   );
 
