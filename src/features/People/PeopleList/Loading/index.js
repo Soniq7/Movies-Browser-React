838aaef694f43d672fad6d12c4bb3d44bd648203
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-10 10:26:18 +0200

    Stworzono komponent Loading

---
+++ src/features/People/PeopleList/Loading/index.js
@@ -0,0 +1,10 @@
+import { Loader } from "../../../../common/Loader/styled";
+import { Wrapper } from "./styled";
+
+const Loading = () => (
+  <Wrapper>
+    <Loader />
+  </Wrapper>
+);
+
+export default Loading;
