dee5917ef54aee4c2896bcfa62c401e0d5d0780a
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-10 10:07:48 +0200

    Stworzono komponent success który mapuje filmy z api

---
+++ src/features/Movies/MovieList/Success/index.js
@@ -0,0 +1,26 @@
+import { ListItem, Item, Header, Wrapper } from "./styled";
+import Tile from "../../../../common/Tile";
+
+const Success = ({ results }) => (
+  <Wrapper>
+    <Header>Popular movie</Header>
+    <ListItem>
+      {results
+        ? results.map((movie) => (
+            <Item key={movie.id}>
+              <Tile
+                title={movie.original_title}
+                year={movie.release_date.substring(0, 4)}
+                rating={movie.vote_average}
+                votes={movie.vote_count}
+                poster={movie.poster_path}
+                genres={"."}
+              />
+            </Item>
+          ))
+        : null}
+    </ListItem>
+  </Wrapper>
+);
+
+export default Success;
