e9375ddec1345bc8a88d0beb0b36757b7829706d
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-10 10:27:57 +0200

    Stworzono people list

---
+++ src/features/People/PeopleList/index.js
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPeople, selectPeople, selectLoading } from "./peopleSlice";
+import { useEffect } from "react";
+import Success from "./Success";
+import Loading from "./Loading";
+import Error from "../../Error";
+
+const PeopleList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPeople());
+  }, []);
+  const people = useSelector(selectPeople);
+  const { results } = people;
+  const state = useSelector(selectLoading);
+
+  switch (state) {
+    case "loading":
+      return <Loading />;
+    case "success":
+      return <Success results={results} />;
+    default:
+      return <Error />;
+  }
+};
+
+export default PeopleList;
