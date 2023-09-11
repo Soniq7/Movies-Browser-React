606e36c3b1dae6421daabd040decc93c5496bcd1
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-11 21:10:06 +0200

    dodano responsywność do kafelków

--- src/common/Tile/styled.js
+++ src/common/Tile/styled.js
@@ -1,27 +1,44 @@
 import styled from "styled-components";
 
-export const Tiled = styled.div`
+export const StyledTile = styled.div`
   background: ${({ theme }) => theme.colors.boxBackground};
   padding: 16px;
   width: 324px;
-  height: 650px;
   box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.5);
   border-radius: 5px;
+  @media (max-width: 700px) {
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 16px;
+    width: auto;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    max-height: 201px;
+    max-width: 445px;
+  }
 `;
 
 export const TileImage = styled.img`
   width: 292px;
   height: 434px;
   border-radius: 5px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 114px;
+    height: 169px;
+  }
 `;
 
-export const TileContent = styled.div``;
-
 export const TileHeader = styled.div`
+  height: auto;
   font-size: 22px;
   font-weight: 500;
   color: ${({ theme }) => theme.colors.primaryText};
-  margin-bottom: 8px;
+  @media (max-width: 700px) {
+    font-size: 26px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 16px;
+  }
 `;
 
 export const TileYear = styled.div`
@@ -29,16 +46,26 @@ export const TileYear = styled.div`
   font-weight: 400;
   color: ${({ theme }) => theme.colors.text.secondaryText};
   padding-left: 0;
-  margin-bottom: 8px;
+  grid-column: 2;
+  @media (max-width: 700px) {
+    font-size: 20px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 13px;
+  }
 `;
 
 export const TileGenres = styled.ul`
+  grid-column: 2;
   display: flex;
   flex-wrap: wrap;
   list-style: none;
   padding-left: 0;
   margin: -4px;
-  margin-bottom: 39px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    gap: 8px;
+    margin-bottom: 8px;
+  }
 `;
 
 export const TileTags = styled.li`
@@ -48,11 +75,27 @@ export const TileTags = styled.li`
   margin: 4px;
   padding: 8px 16px;
   border-radius: 5px;
+  grid-row: 1;
+  @media (max-width: 700px) {
+    padding: 12px 24px;
+    font-size: 18px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    padding: 4px 8px;
+    font-size: 10px;
+    margin: 0;
+  }
 `;
 
 export const TileView = styled.div`
+  grid-column: 2;
   display: flex;
-  flex: wrap;
+  gap: 12px;
+  margin-top: auto;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    margin: 0;
+    gap: 8px;
+  }
 `;
 
 export const TileRating = styled.div`
@@ -60,11 +103,26 @@ export const TileRating = styled.div`
   font-weight: 600;
   text-align: center;
   padding-left: 6px;
+  @media (max-width: 700px) {
+    font-size: 20px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 13px;
+  }
 `;
 
 export const TileRatingIcon = styled.img`
   width: 24px;
   height: 24px;
+  @media (max-width: 700px) {
+    width: 28px;
+    height: 28px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 16px;
+    height: 16px;
+  }
 `;
 
 export const TileVotes = styled.div`
@@ -72,4 +130,18 @@ export const TileVotes = styled.div`
   font-weight: 400;
   color: ${({ theme }) => theme.colors.text.secondaryText};
   padding-left: 6px;
+  white-space: nowrap;
+  @media (max-width: 700px) {
+    font-size: 20px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 13px;
+  }
+`;
+
+export const TileWrapper = styled.span`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  height: 180px;
 `;
