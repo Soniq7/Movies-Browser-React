7540e140895b0955930f45eaa2b259a8f5f9f405
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-13 20:02:53 +0200

    stworzono funkcję pobierającą dane osoby

---
+++ src/features/People/PersonPage/getPerson.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTQ1NTc2MWVhMDhkODdmMzM2NzhmNjUyMjRhMDA3MyIsInN1YiI6IjY0Zjc3OWQ1OGUyMGM1MGNkN2YzOTY3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J-sFQnatRfH853jfmr4C8Wg8P-E-_5XdO5Fl9CLW-yw",
+  },
+};
+
+export const getPerson = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/person/${id}?language=en-US`,
+      options
+    );
+    if (response.status !== 200) {
+      return "error";
+    }
+    return response.data;
+  } catch {
+    return "error";
+  }
+};
