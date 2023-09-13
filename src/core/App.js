d343315497e87a7ec38a4ce50c06767ed6e6a329
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-13 17:01:28 +0200

    Dodano nowe ścieżki dla routera

--- src/core/App.js
+++ src/core/App.js
@@ -6,15 +6,23 @@ import {
 } from "react-router-dom/cjs/react-router-dom";
 import Navigation from "../Navigation";
 import MovieList from "../features/Movies/MovieList";
+import MoviePage from "../features/Movies/MoviePage";
 import PeopleList from "../features/People/PeopleList";
-import { toMovies, toPeople } from "../routes";
+import PersonPage from "../features/People/PersonPage";
+import { toMovies, toMovie, toPeople, toPerson } from "../routes";
 
 export default () => (
   <HashRouter>
     <Navigation />
     <Switch>
+      <Route path={toMovie()}>
+        <MoviePage />
+      </Route>
       <Route path={toMovies()}>
         <MovieList />
+      </Route>
+      <Route path={toPerson()}>
+        <PersonPage />
       </Route>
       <Route path={toPeople()}>
         <PeopleList />
