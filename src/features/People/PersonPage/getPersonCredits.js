ebc431d639c664ecde90a83c177e492494d56d45
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-15 17:15:54 +0200

    created getPersonCredits function

---
+++ src/features/People/PersonPage/getPersonCredits.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTQ1NTc2MWVhMDhkODdmMzM2NzhmNjUyMjRhMDA3MyIsInN1YiI6IjY0Zjc3OWQ1OGUyMGM1MGNkN2YzOTY3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J-sFQnatRfH853jfmr4C8Wg8P-E-_5XdO5Fl9CLW-yw",
+  },
+};
+
+export const getPersonCredits = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/person/${id}/movie_credits?language=en-US`,
+      options
+    );
+    if (response.status !== 200) {
+      return "error";
+    }
+    const { crew, cast } = response.data;
+    return {
+      crew,
+      cast,
+    };
+  } catch {
+    return "error";
+  }
+};
