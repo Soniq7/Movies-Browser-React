c6035deda16b265736bae0996e3cf51c6b3066ee
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-15 17:17:42 +0200

    Created person page

--- src/features/People/PersonPage/Success/index.js
+++ src/features/People/PersonPage/Success/index.js
@@ -1,3 +1,58 @@
-const Success = ({ personData }) => <></>;
+import PageTile from "../../../../common/PageTile";
+import Section from "../../../../common/Section";
+import Tile from "../../../../common/Tile";
+
+const Success = ({ personData, personCreditsData }) => {
+  let cast = [];
+  let crew = [];
+
+  if (personCreditsData) {
+    crew = personCreditsData.crew;
+    cast = personCreditsData.cast;
+  }
+
+  console.log(cast);
+
+  return (
+    <>
+      <Section
+        content={
+          personData ? (
+            <PageTile
+              name={personData.name}
+              description={personData.biography}
+              dateOfBirth={personData.birthday}
+              placeOfBirth={personData.place_of_birth}
+              image={personData.profile_path}
+            />
+          ) : (
+            ""
+          )
+        }
+      />
+
+      {cast ? (
+        <Section
+          header={`Movies - cast (${cast.length})`}
+          content={
+            cast
+              ? cast.map((role) => (
+                  <Tile
+                    poster={role.poster_path}
+                    title={role.title}
+                    genres={"."}
+                    rating={role.vote_average}
+                    votes={role.vote_count}
+                  />
+                ))
+              : ""
+          }
+        />
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
 
 export default Success;
