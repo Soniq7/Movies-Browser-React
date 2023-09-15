11b138c2baac0c036304529747db72b2b32ec6a9
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-15 17:16:30 +0200

    added main to app

--- src/core/App.js
+++ src/core/App.js
@@ -4,6 +4,7 @@ import {
   Route,
   Switch,
 } from "react-router-dom/cjs/react-router-dom";
+import { Main } from "./main";
 import Navigation from "../Navigation";
 import MovieList from "../features/Movies/MovieList";
 import MoviePage from "../features/Movies/MoviePage";
@@ -14,22 +15,24 @@ import { toMovies, toMovie, toPeople, toPerson } from "../routes";
 export default () => (
   <HashRouter basename="movies-browser/">
     <Navigation />
-    <Switch>
-      <Route path={toMovie()}>
-        <MoviePage />
-      </Route>
-      <Route path={toMovies()}>
-        <MovieList />
-      </Route>
-      <Route path={toPerson()}>
-        <PersonPage />
-      </Route>
-      <Route path={toPeople()}>
-        <PeopleList />
-      </Route>
-      <Route path="/">
-        <Redirect to={toMovies()} />
-      </Route>
-    </Switch>
+    <Main>
+      <Switch>
+        <Route path={toMovie()}>
+          <MoviePage />
+        </Route>
+        <Route path={toMovies()}>
+          <MovieList />
+        </Route>
+        <Route path={toPerson()}>
+          <PersonPage />
+        </Route>
+        <Route path={toPeople()}>
+          <PeopleList />
+        </Route>
+        <Route path="/">
+          <Redirect to={toMovies()} />
+        </Route>
+      </Switch>
+    </Main>
   </HashRouter>
 );
