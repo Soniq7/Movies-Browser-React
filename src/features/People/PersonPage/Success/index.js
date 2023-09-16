c8c83a721a76b59af9c40804f86004e978b75191
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-17 00:25:45 +0200

    created person page

--- src/features/People/PersonPage/Success/index.js
+++ src/features/People/PersonPage/Success/index.js
@@ -1,6 +1,7 @@
 import PageTile from "../../../../common/PageTile";
 import Section from "../../../../common/Section";
 import Tile from "../../../../common/Tile";
+import { Item, List } from "./styled";
 
 const Success = ({ personData, personCreditsData }) => {
   let cast = [];
@@ -19,6 +20,7 @@ const Success = ({ personData, personCreditsData }) => {
         content={
           personData ? (
             <PageTile
+              isPersonTile={true}
               name={personData.name}
               description={personData.biography}
               dateOfBirth={personData.birthday}
@@ -35,17 +37,48 @@ const Success = ({ personData, personCreditsData }) => {
         <Section
           header={`Movies - cast (${cast.length})`}
           content={
-            cast
-              ? cast.map((role) => (
-                  <Tile
-                    poster={role.poster_path}
-                    title={role.title}
-                    genres={"."}
-                    rating={role.vote_average}
-                    votes={role.vote_count}
-                  />
-                ))
-              : ""
+            <List>
+              {cast
+                ? cast.map((role) => (
+                    <Item key={role.id}>
+                      <Tile
+                        poster={role.poster_path}
+                        title={role.title}
+                        role={role.character}
+                        genres={"."}
+                        rating={role.vote_average}
+                        votes={role.vote_count}
+                      />
+                    </Item>
+                  ))
+                : ""}
+            </List>
+          }
+        />
+      ) : (
+        ""
+      )}
+
+      {crew ? (
+        <Section
+          header={`Movies - crew (${crew.length})`}
+          content={
+            <List>
+              {crew
+                ? crew.map((role) => (
+                    <Item key={role.id}>
+                      <Tile
+                        poster={role.poster_path}
+                        title={role.title}
+                        job={role.job}
+                        genres={"."}
+                        rating={role.vote_average}
+                        votes={role.vote_count}
+                      />
+                    </Item>
+                  ))
+                : ""}
+            </List>
           }
         />
       ) : (
---
+++ src/features/People/PersonPage/Success/styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  justify-content: center;
+  gap: 24px;
+  padding: 0;
+  margin: 0;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontal}) {
+    grid-template-columns: repeat(3, auto);
+  }
+  @media (max-width: 1070px) {
+    grid-template-columns: repeat(2, auto);
+  }
+  @media (max-width: 700px) {
+    grid-template-columns: auto;
+    grid-gap: 16px;
+  }
+`;
+
+export const Item = styled.li`
+  list-style: none;
+  margin: 0;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 12px;
+  }
+`;
