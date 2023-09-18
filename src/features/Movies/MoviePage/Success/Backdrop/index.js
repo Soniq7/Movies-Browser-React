855f627ff4f7b1f79448ef27febc81c4c4592840
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-18 17:46:08 +0200

    added ratings to backdrop and get width function

--- src/features/Movies/MoviePage/Success/Backdrop/index.js
+++ src/features/Movies/MoviePage/Success/Backdrop/index.js
@@ -5,16 +5,43 @@ import {
   Wrapper,
   Content,
   Title,
+  ReviewIcon,
+  Review,
+  Score,
+  Scale,
+  Votes,
 } from "./styled";
+import { useEffect, useState } from "react";
+import { getWidth } from "../../../../../common/getWidth";
 import shadow from "../../../../../images/shadow.png";
 
 const Backdrop = ({ imagePath, title, score, votes }) => {
+  const [viewportWidth, setViewportWidth] = useState(getWidth());
+
+  useEffect(() => {
+    const updateViewportWidth = () => {
+      setViewportWidth(getWidth());
+    };
+
+    window.addEventListener("resize", updateViewportWidth);
+
+    return () => {
+      window.removeEventListener("resize", updateViewportWidth);
+    };
+  }, [viewportWidth]);
   return (
     <Header>
       <Shadow src={shadow} />
       <Wrapper>
         <Content>
           <Title>{title}</Title>
+          <Review>
+            <ReviewIcon />
+            <Score>{score}</Score>
+            <Scale>/10</Scale>
+            {viewportWidth <= 700 ? <Votes>{votes} votes</Votes> : ""}
+          </Review>
+          {viewportWidth > 700 ? <Votes>{votes} votes</Votes> : ""}
         </Content>
         <BackdropPoster
           src={`https://image.tmdb.org/t/p/original/${imagePath}`}
