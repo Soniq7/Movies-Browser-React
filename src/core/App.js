77775777e1f6cb5b44c7860429a816127e018562
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-18 18:47:29 +0200

    fixed routing issue

--- src/core/App.js
+++ src/core/App.js
@@ -8,18 +8,24 @@ import Navigation from "../Navigation";
 import MovieList from "../features/Movies/MovieList";
 import MoviePage from "../features/Movies/MoviePage";
 import PeopleList from "../features/People/PeopleList";
-import { toMovies, toPeople } from "../routes";
+import PersonPage from "../features/People/PersonPage";
+import { toMovies, toMovie, toPeople, toPerson } from "../routes";
 import PaginationMovie from "../features/Movies/PaginationMovie";
 import PaginationPeople from "../features/People/PaginationPeople";
-
 
 export default () => (
   <HashRouter>
     <Navigation />
     <Switch>
+      <Route path={toMovie()}>
+        <MoviePage />
+      </Route>
       <Route path={toMovies()}>
         <MovieList />
         <PaginationMovie />
+      </Route>
+      <Route path={toPerson()}>
+        <PersonPage />
       </Route>
       <Route path={toPeople()}>
         <PeopleList />
