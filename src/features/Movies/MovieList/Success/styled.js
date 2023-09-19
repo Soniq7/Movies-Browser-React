4ceef81fd75f09a261ccdfa00a9c79f132e76443
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-19 20:14:01 +0200

    less code

--- src/features/Movies/MovieList/Success/styled.js
+++ src/features/Movies/MovieList/Success/styled.js
@@ -1,21 +1,15 @@
 import styled from "styled-components";
 
 export const ListItem = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(4, auto);
+  display: flex;
+  flex-wrap: wrap;
   justify-content: center;
   gap: 24px;
   padding: 0;
   margin: 0;
-  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontal}) {
-    grid-template-columns: repeat(3, auto);
-  }
-  @media (max-width: 1070px) {
-    grid-template-columns: repeat(2, auto);
-  }
+
   @media (max-width: 700px) {
-    grid-template-columns: auto;
-    grid-gap: 16px;
+    gap: 16px;
   }
 `;
 
--- src/features/Movies/MoviePage/Success/styled.js
+++ src/features/Movies/MoviePage/Success/styled.js
@@ -1,23 +1,11 @@
 import styled from "styled-components";
 
 export const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(6, auto);
-  justify-content: center;
-  gap: 24px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
   padding: 0;
   margin: 0;
-  grid-template-rows: auto;
-  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontal}) {
-    grid-template-columns: repeat(4, auto);
-  }
-  @media (max-width: 1070px) {
-    grid-template-columns: repeat(3, auto);
-  }
-  @media (max-width: 700px) {
-    grid-template-columns: auto auto;
-    grid-gap: 16px;
-  }
 `;
 
 export const Item = styled.li`
