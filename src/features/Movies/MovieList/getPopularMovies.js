92691f2d14b089a9318a720aa8d0852396957462
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-19 17:57:08 +0200

    get popularMovies now recieves page variable

--- src/features/Movies/MovieList/getPopularMovies.js
+++ src/features/Movies/MovieList/getPopularMovies.js
@@ -9,9 +9,9 @@ const options = {
   },
 };
 
-export const getPopularMovies = async () => {
+export const getPopularMovies = async (page) => {
   const response = await axios.get(
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_cast=false&with_companies=false&with_crew=false",
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&with_cast=false&with_companies=false&with_crew=false`,
     options
   );
 
