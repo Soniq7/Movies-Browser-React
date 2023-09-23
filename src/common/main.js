5d85fdc811898f821743d43453ad4598ec495e78
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-23 19:37:05 +0200

    added bottom padding to page

--- src/common/main.js
+++ src/common/main.js
@@ -4,15 +4,16 @@ export const PageMain = styled.main`
   display: grid;
   grid-template-columns: minmax(300px, 1350px);
   justify-content: center;
-  padding: 0 20px;
+  padding: 0 20px 330px 0;
   margin: 0 auto;
   align-items: center;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontal}) {
     grid-template-columns: auto;
+    padding: 0 20px 210px 0;
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    padding: 10px;
+    padding: 10px 10px 80px 10px;
   }
 `;
 export const ListMain = styled.main`
