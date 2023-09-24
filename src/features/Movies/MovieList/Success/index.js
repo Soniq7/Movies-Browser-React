02a666671ee11ebe6e680a69a5c397ea437725f9
Soniq7 <ssoniq7@gmail.com>
2023-09-25 00:09:09 +0200

    Success.js code formatting

--- src/features/Movies/MovieList/Success/index.js
+++ src/features/Movies/MovieList/Success/index.js
@@ -8,47 +8,46 @@ import Pagination from "../../../Pagination/index";
 import { selectSearchTerm } from "../moviesSlice";
 
 const Success = ({ results }) => {
-  const [pageTitle, setPageTitle] = useState('Popular movies');
+  const [pageTitle, setPageTitle] = useState("Popular movies");
   const searchTerm = useSelector(selectSearchTerm);
 
   useEffect(() => {
     if (searchTerm) {
       setPageTitle(`Search results for "${searchTerm}" (${results.length})`);
     } else {
-      setPageTitle('Popular movies');
+      setPageTitle("Popular movies");
     }
   }, [searchTerm, results]);
-  
+
   return (
     <>
-    <ListMain>
-      <Section
-        header={pageTitle}
-        content={
-          <ListItem>
-            {results
-              ? results.map((movie) => (
-                  <Item key={movie.id}>
-                    <Tile
-                      name={movie.original_title}
-                      year={movie.release_date.substring(0, 4)}
-                      score={movie.vote_average.toFixed(1)}
-                      votes={movie.vote_count}
-                      image={movie.poster_path}
-                      genres={"."}
-                      id={movie.id}
-                    />
-                  </Item>
-                ))
-              : null}
-          </ListItem>
-        }
-      />
-    </ListMain>
-    <Pagination isMovieList={true} />
-  </>
-
-  )
+      <ListMain>
+        <Section
+          header={pageTitle}
+          content={
+            <ListItem>
+              {results
+                ? results.map((movie) => (
+                    <Item key={movie.id}>
+                      <Tile
+                        name={movie.original_title}
+                        year={movie.release_date.substring(0, 4)}
+                        score={movie.vote_average.toFixed(1)}
+                        votes={movie.vote_count}
+                        image={movie.poster_path}
+                        genres={"."}
+                        id={movie.id}
+                      />
+                    </Item>
+                  ))
+                : null}
+            </ListItem>
+          }
+        />
+      </ListMain>
+      <Pagination isMovieList={true} />
+    </>
+  );
 };
 
 export default Success;
