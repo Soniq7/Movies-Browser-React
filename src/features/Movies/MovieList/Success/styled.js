0d84c08083d304ed236e61624ff587577b820907
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-25 19:55:20 +0200

    added different styles if movie list is short

--- src/features/Movies/MovieList/Success/styled.js
+++ src/features/Movies/MovieList/Success/styled.js
@@ -1,21 +1,31 @@
-import styled from "styled-components";
+import { styled, css } from "styled-components";
 
 export const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(324px, 1fr));
-  justify-content: center;
-  grid-gap: 24px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 24px;
   padding: 0;
   margin: 0;
+  ${({ shortList }) =>
+    shortList &&
+    css`
+      justify-content: center;
+    `}
+  @media (max-width: 1100px) {
+    justify-content: center;
+  }
   @media (max-width: 700px) {
-    grid-template-columns: auto;
-    grid-gap: 16px;
+    gap: 16px;
   }
 `;
 
 export const Item = styled.li`
   list-style: none;
   margin: 0;
+  @media (max-width: 750px) {
+    width: 100%;
+  }
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: 12px;
   }
