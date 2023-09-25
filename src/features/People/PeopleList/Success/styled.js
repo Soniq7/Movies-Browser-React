21200415dfb60977a00e32bac5b86762ce814fb9
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2023-09-25 20:05:39 +0200

    added shortlist attribute to people list also

--- src/features/People/PeopleList/Success/styled.js
+++ src/features/People/PeopleList/Success/styled.js
@@ -1,16 +1,27 @@
-import styled from "styled-components";
+import { styled, css } from "styled-components";
 
 export const List = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(208px, 1fr));
-  justify-content: center;
+  grid-template-columns: repeat(auto-fit, 208px);
+  justify-content: space-between;
   gap: 24px;
   padding: 0;
   margin: 0;
+  ${({ shortList }) =>
+    shortList &&
+    css`
+      justify-content: start;
+    `}
+  @media (max-width: 750px) and (min-width: 550px) {
+    justify-content: space-around;
+  }
   @media (max-width: 700px) {
     gap: 16px;
   }
-  @media (max-width: 550px) {
+  @media (max-width: 520px) {
+    justify-content: space-around;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     grid-template-columns: repeat(auto-fit, 136px);
   }
 `;
