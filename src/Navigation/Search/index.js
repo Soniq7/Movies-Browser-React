c982e4215a1cdc25d6a89a403f888e3415bca358
Soniq7 <ssoniq7@gmail.com>
2023-10-01 19:22:10 +0200

    setting search results based on URL parameters

--- src/Navigation/Search/index.js
+++ src/Navigation/Search/index.js
@@ -1,36 +1,47 @@
-import { useState, useImperativeHandle, forwardRef } from "react";
+import { useState, useImperativeHandle, forwardRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Input, LoupeIcon, Wrapper, IconWrapper } from "./styled";
 import { useQueryParameter, useReplaceQueryParameter } from "./queryParameters";
 import searchQueryParamName from "./searchQueryParamName";
-import { updateMoviesSearchTerm } from "../../features/Movies/MovieList/moviesSlice";
-import { updatePeopleSearchTerm } from "../../features/People/PeopleList/peopleSlice";
+import {
+  selectMoviesSearchTerm,
+  updateMoviesSearchTerm,
+} from "../../features/Movies/MovieList/moviesSlice";
+import {
+  selectPeopleSearchTerm,
+  updatePeopleSearchTerm,
+} from "../../features/People/PeopleList/peopleSlice";
 
 const Search = forwardRef((props, ref) => {
   const location = useLocation();
   const dispatch = useDispatch();
+  const moviesSearchTerm = useSelector(selectMoviesSearchTerm);
+  const peopleSearchTerm = useSelector(selectPeopleSearchTerm);
 
   const query = useQueryParameter(searchQueryParamName);
   const replaceQueryParameter = useReplaceQueryParameter();
 
   const [localSearchTerm, setLocalSearchTerm] = useState(query || "");
 
+  useEffect(() => {
+    if (location.pathname.startsWith("/movies")) {
+      dispatch(updateMoviesSearchTerm(localSearchTerm));
+    } else if (location.pathname.startsWith("/people")) {
+      dispatch(updatePeopleSearchTerm(localSearchTerm));
+    }
+  }, [localSearchTerm, location.pathname, dispatch]);
+
   const onInputChange = (event) => {
     const value = event.target.value;
-    dispatch(updateMoviesSearchTerm(value));
-    dispatch(updatePeopleSearchTerm(value));
     setLocalSearchTerm(value);
     replaceQueryParameter({
       key: searchQueryParamName,
       value: value.trim() !== "" ? value : undefined,
     });
   };
-
   const resetSearch = () => {
     setLocalSearchTerm("");
-    dispatch(updateMoviesSearchTerm(""));
-    dispatch(updatePeopleSearchTerm(""));
     replaceQueryParameter({
       key: searchQueryParamName,
       value: "",
@@ -41,11 +52,9 @@ const Search = forwardRef((props, ref) => {
     resetSearch,
   }));
 
-  const pathname = location.pathname;
-
-  const placeholderText = pathname.startsWith("/movies")
+  const placeholderText = location.pathname.startsWith("/movies")
     ? "Search for movies..."
-    : pathname.startsWith("/people")
+    : location.pathname.startsWith("/people")
     ? "Search for people..."
     : "";
 
